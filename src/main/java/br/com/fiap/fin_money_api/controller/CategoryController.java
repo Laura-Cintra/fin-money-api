92fa236338be9d5da67ab217e9fd09ece7ead91f
Laura Cintra <79454879+Laura-Cintra@users.noreply.github.com>
2025-03-06 11:58:41 -0300

    Adicionando métodos DELETE e PUT
    Melhorando construção dos métodos

--- src/main/java/br/com/fiap/fin_money_api/controller/CategoryController.java
+++ src/main/java/br/com/fiap/fin_money_api/controller/CategoryController.java
@@ -3,21 +3,30 @@ package br.com.fiap.fin_money_api.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 // import org.springframework.web.bind.annotation.RequestMapping;
 // import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.fiap.fin_money_api.model.Category;
 
 @RestController
+@RequestMapping("/categories") // como todos os métodos usavam o mesmo caminho
 public class CategoryController {
+
+    private Logger log = LoggerFactory.getLogger(getClass()); // objeto para log no terminal
 
     private List<Category> repository = new ArrayList<>(); // banco de dados em memória
 
@@ -25,33 +34,24 @@ public class CategoryController {
     // {RequestMethod.GET})
     // mapeia requisições para este método
 
-    @GetMapping("/categories") // mapeia requisições do tipo GET, path é um atributo padrão, por isso omitimos
+    @GetMapping() // mapeia requisições do tipo GET, path é um atributo padrão, por isso omitimos
     public List<Category> index() {
         return repository;
     }
 
-    @PostMapping("/categories")
+    @PostMapping()
     @ResponseStatus(code = HttpStatus.CREATED) // se o método foi criado com sucesso, a mensagem será "created"
     public Category create(@RequestBody Category category) { // mostrando que a categoria estará no corpo da requisição
-        System.out.println("Cadastrando categoria " + category.getName());
+        log.info("Cadastrando categoria " + category.getName());
         repository.add(category);
         return category;
     }
 
-    @GetMapping("/categories/{id}")
-    public ResponseEntity<Category> get(@PathVariable Long id) { // ResponseEntity - eu controlo o que vai ser enviado
-                                                                 // na resposta
-        System.out.println("Buscando categoria " + id);
-        var category = repository.stream()
-                .filter(c -> c.getId().equals(id))
-                .findFirst(); // stream - fluxo de dados, pega uma coleção e retorna um fluxo que pode ser
-                              // consumido por uma outra função
-
-        if (category.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        return ResponseEntity.ok(category.get());
+    @GetMapping("{id}")
+    // ResponseEntity - eu controlo o que vai ser enviado na resposta
+    public ResponseEntity<Category> get(@PathVariable Long id) {
+        log.info("Buscando categoria " + id);
+        return ResponseEntity.ok(getCategory(id));
 
         /*
          * // verificando se o optional tá vazio
@@ -64,5 +64,32 @@ public class CategoryController {
          * e pegando a categoria dentro dela,
          * // que pode ser
          */
+    }
+
+    // Apagar
+    @DeleteMapping("{id}")
+    public ResponseEntity<Object> destroy(@PathVariable Long id) {
+        log.info("Apagando categoria " + id);
+        repository.remove(getCategory(id)); // usando direto porque não precisa de uma variável que só vai usar 1 vez
+        return ResponseEntity.noContent().build(); // o retorno de sucesso é 204
+    }
+
+    // Editar
+    @PutMapping("{id}")
+    public ResponseEntity<Object> update(@PathVariable Long id, @RequestBody Category category) {
+        log.info("Atualizando categoria + " + id + " " + category);
+
+        repository.remove(getCategory(id)); // tirando os dados antigos
+        category.setId(id); // evitando que o id seja atualizado
+        repository.add(category); // adicionando os novos dados
+        return ResponseEntity.ok(category);
+    }
+
+    private Category getCategory(Long id) {
+        // stream - fluxo de dados, pega uma coleção e retorna um fluxo que pode ser
+        // consumido por uma outra função
+        return repository.stream().filter(c -> c.getId().equals(id)).findFirst().orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Categoria não encontrada"));
+        // pega o primeiro ou, senão, lança uma exceção
     }
 }
