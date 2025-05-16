9a927129d02587ef1f389c0492429b465f47dc20
Laura Cintra <lauraocintra@gmail.com>
2025-05-16 12:01:23 -0300

    feat: spring ai

--- src/main/java/br/com/fiap/fin_money_api/repository/TransactionRepository.java
+++ src/main/java/br/com/fiap/fin_money_api/repository/TransactionRepository.java
@@ -1,12 +1,17 @@
 package br.com.fiap.fin_money_api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 // import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import br.com.fiap.fin_money_api.model.Transaction;
+import br.com.fiap.fin_money_api.model.TransactionType;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long>, JpaSpecificationExecutor<Transaction>{
 
     // Repositório JPA para a entidade Transaction
+
+    List<Transaction> findByType(TransactionType type);
 }
